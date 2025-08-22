afd774d440cb567f7917b1d596145aa88b1847e1
baou9 <58894833+baou9@users.noreply.github.com>
2025-08-22 11:52:40 +0100

    Delete app directory

--- app/api/error_codes.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Small mapping of Huawei alarm codes to human-readable messages
-return [
-    '100001' => 'Grid overvoltage',
-    '100002' => 'Grid undervoltage',
-    '200001' => 'Device offline',
-];
