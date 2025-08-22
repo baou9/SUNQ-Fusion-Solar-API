41b786f98defe3c55aa3ed605b152ef2a12e9630
baou9 <58894833+baou9@users.noreply.github.com>
2025-08-22 11:52:57 +0100

    Delete backend directory

--- backend/lib/alarmCodes.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '1001': { message: 'Overvoltage', severity: 'major' },
-  '1002': { message: 'Undervoltage', severity: 'minor' },
-};
--- backend/test/greenMetrics.test.js
+++
@@ -1,8 +0,0 @@
-const { greenMetrics, CO2_FACTOR_KG_PER_KWH, TREE_CO2_KG_PER_YEAR, HOME_KWH_PER_DAY } = require('../lib/greenMetrics');
-
-test('greenMetrics calculates expected values', () => {
-  const res = greenMetrics(100);
-  expect(res.co2AvoidedKg).toBeCloseTo(100 * CO2_FACTOR_KG_PER_KWH);
-  expect(res.treesEquivalent).toBeCloseTo(res.co2AvoidedKg / TREE_CO2_KG_PER_YEAR);
-  expect(res.homesPowered).toBeCloseTo(100 / HOME_KWH_PER_DAY);
-});
